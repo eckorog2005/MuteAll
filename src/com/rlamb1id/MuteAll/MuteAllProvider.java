c5443480fd4e6938a3976bebee154dd29d6d16ba
Roger Lamb <eckorog2005@gmail.com>
2014-02-21 21:27:16 -0500

    started widget work

---
+++ src/com/rlamb1id/MuteAll/MuteAllProvider.java
@@ -0,0 +1,32 @@
+package com.rlamb1id.MuteAll;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by rlamb on 2/21/14.
+ */
+public class MuteAllProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+    @Override
+    public void onUpdate (Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+
+    }
+
+    @Override
+    public void onEnabled (Context context){
+
+    }
+
+    @Override
+    public void onDisabled (Context context){
+
+    }
+}
